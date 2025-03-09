35793debb1f36cc80b6e8850d2753738c013d40b
MinhPhung <139702411+minhphung2911@users.noreply.github.com>
2025-03-09 12:50:40 +0700

    Up

---
+++ src/test/java/com/example/webgiatui/WebGiatUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webgiatui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGiatUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
