a7fff5099f52e4a67af703b3922f4a56b36a20e8
TTC2042005 <162353629+TTC2042005@users.noreply.github.com>
2025-03-30 23:10:25 +0700

    Add files via upload
    tạo 2 class user để người đung đăng nhập dăng ký và class order để người dùng đặt dịch vụ của cửa hàng

---
+++ User.java
@@ -0,0 +1,12 @@
+package com.example.webgiatui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.webgiatui.model.User;
+import com.example.webgiatui.model.Order;
+
+public class WebGiatUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebGiatUiApplication.class, args);
+    }
+}
