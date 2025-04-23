cdff510f698ba4dd5e8e418e88500e5c0992158a
MinhPhung <kd6763658@gmail.com>
2025-04-23 22:38:50 +0700

    update again

--- src/main/java/com/example/webgiatui/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.webgiatui.repository;
-
-import com.example.webgiatui.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- src/main/java/com/example/webgiatui/service/LynService.java
+++
@@ -1,7 +0,0 @@
-package com.example.webgiatui.service;
-
-import com.example.webgiatui.entity.Service;
-import java.util.List;
-
-public interface LynService extends GenericService<Service> {
-}
